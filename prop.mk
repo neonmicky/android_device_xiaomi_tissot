57b28935703be615e37d4a49bb496b8dd9c06958
TogoFire <italomellopereira@gmail.com>
2020-11-17 17:23:07 +0530

    tissot: readjustment hal1 packagelist
    Signed-off-by: neonmicky <neonmicky30@gmail.com>

--- prop.mk
+++ prop.mk
@@ -11,7 +11,7 @@ vendor.camera.aux.packageblacklist=com.discord \
 persist.camera.HAL3.enabled=1 \
 vendor.camera.aux.packagelist=org.lineageos.snap,com.android.camera,com.google.android.GoogleCamera \
 vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
-vendor.camera.hal1.packagelist=com.whatsapp \
+vendor.camera.hal1.packagelist=com.android \
 persist.camera.dual.camera=0 \
 persist.camera.eis.enable=1
 
